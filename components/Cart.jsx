3dd99d235fa3632959f4516a24558d98101c8106
mayankgera0 <mayankgera79@gmail.com>
2023-12-14 01:04:18 +0530

    updated

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -57,7 +57,7 @@ const Cart = () => {
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
-                  <h4>${item.price}</h4>
+                  <h4>₹{item.price}</h4>
 
                 </div>
                 <div className="flex bottom">
@@ -89,7 +89,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>${totalPrice}</h3>
+              <h3>₹{totalPrice}</h3>
 
             </div>
             <div className="btn-container">
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,17 +11,16 @@ export default async function handler(req, res) {
             payment_method_types: ['card'],
             billing_address_collection: 'auto',
             shipping_options: [
-                {shipping_rate: 'shr_1M7FT1SCUP34Mjkzmpb9C7kP'},
-                {shipping_rate: 'shr_1M72X9SCUP34MjkzrRMV5GDP'},
+                {shipping_rate: 'shr_1OMxczSJ5Cti6MkbkE1mY5vS'}
                 
             ],
             line_items: req.body.map((item) => {
                 const img = item.image[0].asset._ref;
-                const newImage = img.replace('image-', 'https://cdn.sanity.io/images/bygto9kv/production/').replace('-webp', '.webp');
+                const newImage = img.replace('image-', 'https://cdn.sanity.io/images/90qewvbm/production/').replace('-webp', '.webp');
                 
                 return {
                     price_data: {
-                        currency: 'usd',
+                        currency: 'inr',
                         product_data: {
                             name: item.name,
                             images: [newImage],
